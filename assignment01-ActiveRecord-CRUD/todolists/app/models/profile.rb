d87dac280cfa2a812aa6b4a49aa857daaff0e04d
CausalReinforcer <flyupupup@gmail.com>
2018-03-26 10:06:15 -0700

    delete old directory

--- assignment01-ActiveRecord-CRUD/todolists/app/models/profile.rb
+++
@@ -1,7 +0,0 @@
-class Profile < ActiveRecord::Base
-  validates :gender,
-            inclusion: {
-              in: %w[female male],
-              message: '%{value} is not a valid relationship status'
-            }
-end
