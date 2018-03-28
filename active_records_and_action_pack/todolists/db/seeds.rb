56c6c26c55aac4cf9e98f059d3b82b973a32bad9
CausalReinforcer <flyupupup@gmail.com>
2018-03-28 01:54:54 -0700

    completed test rq09

--- active_records_and_action_pack/todolists/db/seeds.rb
+++ active_records_and_action_pack/todolists/db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.destroy_all
+Profile.destroy_all
+TodoList.destroy_all
+TodoItem.destroy_all
+
+User.create! [
+  { username: 'Fiorina', password_digest: '#1' },
+  { username: 'Trump', password_digest: '#2' },
+  { username: 'Carson', password_digest: '#3' },
+  { username: 'Clinton', password_digest: '#4' }
+]
+
+Profile.create! [
+  { gender: 'female', birth_year: 1954,
+    first_name: 'Carly', last_name: 'Fiorina', user_id: 1 },
+  { gender: 'male', birth_year: 1946,
+    first_name: 'Donald', last_name: 'Trump', user_id: 2 },
+  { gender: 'male', birth_year: 1951,
+    first_name: 'Ben', last_name: 'Carson', user_id: 3 },
+  { gender: 'female', birth_year: 1947,
+    first_name: 'Hillary', last_name: 'Clinton', user_id: 4 }
+]
+
+User.all.each do |user|
+  user.todo_lists.create! [
+    { list_name: user[:username] + ' list ', list_due_date: Date.today + 1.year }
+  ]
+end
+
+TodoList.all.each do |list|
+  5.times do |i|
+    list.todo_items.create! [
+      { title: list[:id].to_s + ' item ' + i.to_s,
+        due_date: Date.today + 1.year,
+        description: list[:id].to_s + ' item ' + i.to_s,
+        completed: false }
+    ]
+  end
+end
