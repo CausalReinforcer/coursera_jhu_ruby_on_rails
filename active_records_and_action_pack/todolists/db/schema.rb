1732545094d67cd9098679262d593bb9aafbb37f
CausalReinforcer <flyupupup@gmail.com>
2018-04-01 00:26:57 -0700

    week 3 first commit

--- active_records_and_action_pack/todolists/db/schema.rb
+++ active_records_and_action_pack/todolists/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180325170803) do
+ActiveRecord::Schema.define(version: 20180401065708) do
 
   create_table "profiles", force: :cascade do |t|
     t.string   "gender"
--- active_records_and_action_pack/todolists/db/seeds.rb
+++ active_records_and_action_pack/todolists/db/seeds.rb
@@ -1,47 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+today = Date.today
+two_days_ago = Date.today - 2.days
+three_days_ago = Date.today - 3.days
 
-User.destroy_all
-Profile.destroy_all
-TodoList.destroy_all
 TodoItem.destroy_all
 
-User.create! [
-  { username: 'Fiorina', password_digest: '#1' },
-  { username: 'Trump', password_digest: '#2' },
-  { username: 'Carson', password_digest: '#3' },
-  { username: 'Clinton', password_digest: '#4' }
+TodoItem.create! [
+  { title: "Task 1", due_date: today, description: "very important task TEST", completed: false },
+  { title: "Task 2", due_date: two_days_ago, description: "learn ruby TEST", completed: true},
+  { title: "Task 3", due_date: three_days_ago, description: "learn Active Record TEST", completed: true}
 ]
-
-Profile.create! [
-  { gender: 'female', birth_year: 1954,
-    first_name: 'Carly', last_name: 'Fiorina', user_id: 1 },
-  { gender: 'male', birth_year: 1946,
-    first_name: 'Donald', last_name: 'Trump', user_id: 2 },
-  { gender: 'male', birth_year: 1951,
-    first_name: 'Ben', last_name: 'Carson', user_id: 3 },
-  { gender: 'female', birth_year: 1947,
-    first_name: 'Hillary', last_name: 'Clinton', user_id: 4 }
-]
-
-User.all.each do |user|
-  user.todo_lists.create! [
-    { list_name: user[:username] + ' list ', list_due_date: Date.today + 1.year }
-  ]
-end
-
-TodoList.all.each do |list|
-  5.times do |i|
-    list.todo_items.create! [
-      { title: list[:id].to_s + ' item ' + i.to_s,
-        due_date: Date.today + 1.year,
-        description: list[:id].to_s + ' item ' + i.to_s,
-        completed: false }
-    ]
-  end
-end
