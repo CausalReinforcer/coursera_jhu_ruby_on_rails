0d99b877a8e51818fc999a26e091f39efd8d3cf9
CausalReinforcer <flyupupup@gmail.com>
2018-04-15 13:38:03 -0700

    Assignment 4 Requirement 1-5.

--- active_records_and_action_pack/todolists/db/seeds.rb
+++ active_records_and_action_pack/todolists/db/seeds.rb
@@ -1,11 +1,27 @@
 today = Date.today
 two_days_ago = Date.today - 2.days
 three_days_ago = Date.today - 3.days
+dates = [today, two_days_ago, three_days_ago]
 
-TodoItem.destroy_all
+User.destroy_all
+TodoList.destroy_all
 
-TodoItem.create! [
-  { title: "Task 1", due_date: today, description: "very important task TEST", completed: false },
-  { title: "Task 2", due_date: two_days_ago, description: "learn ruby TEST", completed: true},
-  { title: "Task 3", due_date: three_days_ago, description: "learn Active Record TEST", completed: true}
+100.times { |index| TodoList.create! list_name: "List #{index}", list_due_date: dates.sample }
+
+TodoList.all.each do |list|
+  list.todo_items.create! [
+    { title: "Task 1", due_date: dates.sample, description: "very important task TEST", completed: false },
+    { title: "Task 2", due_date: dates.sample, description: "do something else TEST", completed: true},
+    { title: "Task 3", due_date: dates.sample, description: "learn Action Pack TEST", completed: true}
+  ]
+end
+
+users = User.create! [
+  { username: "jim", password: "abc123" },
+  { username: "rich", password: "123abc" }
 ]
+
+TodoList.all.each do |list|
+  list.user = users.sample
+  list.save!
+end
